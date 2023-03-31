1abd0f72aaaf87efa6f73b31c1277b119b670ac9
Bjarki Baldursson Harksen <bjarki31@gmail.com>
2023-03-31 01:16:55 +0000

    fix: remove commented code

--- app/Main.hs
+++ app/Main.hs
@@ -8,21 +8,11 @@ import Language.LambdaCalculus (Var(..), Term(..))
 import Language.LambdaCalculus.Parser (parseTerm, ParserResult(..))
 import Language.SimpleTypes.Inference (principalJudgement)
 
--- printType :: Term -> IO ()
--- printType m =
---   case principalJudgement m of
---        Nothing -> putStrLn $ show m ++ " is not typable"
---        Just j  -> print j
-
 isExitStr :: String -> Bool
 isExitStr = flip elem [":exit", ":quit", ":x", ":q"]
 
 interrupt :: InputT IO a -> InputT IO a -> InputT IO a
 interrupt f = handleInterrupt f . withInterrupt
-
--- getInputWith :: String -> InputT IO (Maybe String)
--- getInputWith "" = getInputLine "λ> "
--- getInputWith _  = getInputLine ".. "
 
 repl :: String -> InputT IO ()
 repl prev = do
@@ -35,7 +25,6 @@ repl prev = do
       case parseTerm s of
         Error err -> outputStrLn err >> repl ""
         Incomplete -> repl (s ++ "\n")
-        -- Success m -> do
         Success m -> do
           msg <- return $
             case principalJudgement m of
@@ -53,91 +42,3 @@ tabComplete (l, r) = return (l, [Completion "\t" "" False])
 
 main :: IO ()
 main = runInputT (setComplete tabComplete defaultSettings) startRepl
-
--- main :: IO ()
--- main = do
---   let u = Abs (Var "u") (Abs (Var "x") (App (TermVar (Var "x")) (App (App (TermVar (Var "u")) (TermVar (Var "u"))) (TermVar (Var "x")))))
---   printType $ App (TermVar (Var "x")) (TermVar (Var "x"))
---   printType $ u
---   printType $ App u u
---   printType $ App (TermVar (Var "x")) (TermVar (Var "y"))
---   printType $ App (Abs (Var "x") (TermVar (Var "x"))) (Abs (Var "x") (TermVar (Var "y")))
---   printType $ Abs (Var "x") (Abs (Var "y") (Abs (Var "z") (App (TermVar (Var "x")) (App (TermVar (Var "y")) (TermVar (Var "z"))))))
---   printType $ TermVar (Var "y")
---   printType $ Abs (Var "z") (TermVar (Var "y"))
---   printType $ Abs (Var "y") (Abs (Var "z") (TermVar (Var "y")))
---   printType $ Abs (Var "x") (Abs (Var "y") (Abs (Var "z") (TermVar (Var "y"))))
---   printType $ Abs (Var "x") $ TermVar (Var "x")
---   printType $ Abs (Var "x") $ Abs (Var "y") $ TermVar (Var "x")
---   printType $ App (Abs (Var "x") $ Abs (Var "y") $ TermVar (Var "y")) (Abs (Var "z") $ TermVar (Var "z"))
---   printType $ Abs (Var "x") (Abs (Var "y") (Abs (Var "z") (App (TermVar (Var "y")) (App (TermVar (Var "x")) (TermVar (Var "z"))))))
---   printType $ Abs (Var "x") (Abs (Var "y") (Abs (Var "z") (App (App (TermVar (Var "x")) (TermVar (Var "z"))) (TermVar (Var "y")))))
---   printType $ Abs (Var "x") (Abs (Var "y") (Abs (Var "z") (App (App (TermVar (Var "x")) (TermVar (Var "z"))) (App (TermVar (Var "y")) (TermVar (Var "z"))))))
---   printType $ Abs (Var "x") (Abs (Var "y") (App (App (TermVar (Var "x")) (TermVar (Var "y"))) (TermVar (Var "y"))))
---   printType $
---     App
---       (Abs (Var "v")
---         (Abs (Var "x")
---           (Abs (Var "y")
---             (Abs (Var "z")
---               (App
---                 (TermVar (Var "v"))
---                 (App
---                   (TermVar (Var "y"))
---                   (App
---                     (App
---                       (TermVar (Var "v"))
---                       (TermVar (Var "x"))
---                     )
---                     (TermVar (Var "z"))
---                   )
---                 )
---               )
---             )
---           )
---         )
---       )
---       (Abs (Var "x")
---         (TermVar (Var "x"))
---       )
---   printType $
---     Abs (Var "x")
---       (Abs (Var "y")
---         (Abs (Var "z")
---           (App
---             (Abs (Var "x")
---               (TermVar (Var "x"))
---             )
---             (App
---               (TermVar (Var "y"))
---               (App
---                 (App
---                   (Abs (Var "x")
---                     (TermVar (Var "x"))
---                   )
---                   (TermVar (Var "x"))
---                 )
---                 (TermVar (Var "z"))
---               )
---             )
---           )
---         )
---       )
---   printType $
---     App
---       (Abs (Var "x")
---         (TermVar (Var "x"))
---       )
---       (App
---         (TermVar (Var "y"))
---         (App
---           (App
---             (Abs (Var "x")
---               (TermVar (Var "x"))
---             )
---             (TermVar (Var "x"))
---           )
---           (TermVar (Var "z"))
---         )
---       )
---
